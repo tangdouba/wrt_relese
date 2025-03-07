42b6907ca78e53ecaf0fab793a134389e566ddee
tangdouba <90525266+tangdouba@users.noreply.github.com>
2025-03-07 19:53:38 +0800

    Update update.sh
    Signed-off-by: tangdouba <90525266+tangdouba@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -597,14 +597,7 @@ EOF
     fi
 }
 
-update_mosdns_deconfig() {
-    local mosdns_conf="$BUILD_DIR/feeds/small8/luci-app-mosdns/root/etc/config/mosdns"
-    if [ -d "${mosdns_conf%/*}" ] && [ -f "$mosdns_conf" ]; then
-        sed -i 's/8000/300/g' "$mosdns_conf"
-    fi
-}
-
-change_up() {
+fix_up() {
     if ! grep -q "LiBwrt" "$BUILD_DIR/include/version.mk"; then
         return
     cd $BUILD_DIR
@@ -619,6 +612,13 @@ change_up() {
     fi
     if [[ -f $BUILD_DIR/package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc ]]; then
         sed -i 's/LiBwrt/OpenWrt/g' $BUILD_DIR/package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+    fi
+}
+
+update_mosdns_deconfig() {
+    local mosdns_conf="$BUILD_DIR/feeds/small8/luci-app-mosdns/root/etc/config/mosdns"
+    if [ -d "${mosdns_conf%/*}" ] && [ -f "$mosdns_conf" ]; then
+        sed -i 's/8000/300/g' "$mosdns_conf"
     fi
 }
 
