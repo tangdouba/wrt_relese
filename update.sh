5254524acf65539a5035d34de634eaee651fb1e2
tangdouba <90525266+tangdouba@users.noreply.github.com>
2025-03-07 19:19:44 +0800

    Update update.sh
    Signed-off-by: tangdouba <90525266+tangdouba@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -663,8 +663,10 @@ main() {
     # set_build_signature
     fix_compile_vlmcsd
     update_nss_diag
-    update_menu_location
     fix_compile_coremark
+    update_homeproxy
+    update_openclash
+    update_menu_location
     update_dnsmasq_conf
     # update_lucky
     add_backup_info_to_sysupgrade
