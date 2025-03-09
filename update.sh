d5e27d18e91f5cd026b0b2460e25814bbc191c5a
tangdouba <90525266+tangdouba@users.noreply.github.com>
2025-03-09 15:05:04 +0800

    Update update.sh
    Signed-off-by: tangdouba <90525266+tangdouba@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -165,8 +165,8 @@ fix_default_set() {
     install -Dm755 "$BASE_PATH/patches/99_set_argon_primary" "$BUILD_DIR/package/base-files/files/etc/uci-defaults/99_set_argon_primary"
 
     if [ -f "$BUILD_DIR/feeds/small8/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg" ]; then
-        if [ -f "$BASE_PATH/images/bg1.jpg" ]; then
-            \cp -f "$BASE_PATH/images/bg1.jpg" "$BUILD_DIR/feeds/small8/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg"
+        if [ -f "$BASE_PATH/patches/bg1.jpg" ]; then
+            \cp -f "$BASE_PATH/patches/bg1.jpg" "$BUILD_DIR/feeds/small8/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg"
         fi
     fi
 
