2a248e6841db2aff171248c9313f9e53bf66a13b
tangdouba <90525266+tangdouba@users.noreply.github.com>
2025-03-11 16:45:46 +0800

    Update update.sh
    Signed-off-by: tangdouba <90525266+tangdouba@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -523,10 +523,11 @@ update_dnsmasq_conf() {
 }
 
 presetset_homeproxy_resources() {
+#预置HomeProxy数据
     local hp_rule="$BUILD_DIR/package/surge"
-    local hp_path="$BUILD_DIR/feeds/small8/luci-app-homeproxy/root/etc/homeproxy"
+    local hp_path="$BUILD_DIR/feeds/small8/luci-app-homeproxy/root/etc/homeproxy/resources"
     if [ -d "$(dirname "$hp_path")" ]; then
-        rm -rf ./$hp_path/resources/*
+        rm -rf ./$hp_path/*
 		
 	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$hp_rule/
 	cd ./$hp_rule/ && res_ver=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
@@ -534,7 +535,7 @@ presetset_homeproxy_resources() {
 	echo $res_ver | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
 	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
 	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
-	mv -f ./{china_*,gfw_list}.{ver,txt} ../$hp_path/resources/
+	mv -f ./{china_*,gfw_list}.{ver,txt} ../$hp_path/
 
 	cd .. && rm -rf ./$hp_rule/
     fi
