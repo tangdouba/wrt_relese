38b79fa60a5f076cc51a1f03605829976db7bb19
tangdouba <90525266+tangdouba@users.noreply.github.com>
2025-04-07 00:54:47 +0800

    Update update.sh
    Signed-off-by: tangdouba <90525266+tangdouba@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -156,7 +156,7 @@ install_small8() {
         luci-app-store quickstart luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest \
         luci-theme-argon netdata luci-app-netdata lucky luci-app-lucky luci-app-openclash luci-app-homeproxy \
         luci-app-amlogic nikki luci-app-nikki tailscale luci-app-tailscale oaf open-app-filter luci-app-oaf \
-        easytier luci-app-easytier msd_lite luci-app-msd_lite qbittorrent luci-app-qbittorrent
+        easytier luci-app-easytier msd_lite luci-app-msd_lite qbittorrent luci-app-qbittorrent luci-app-argon-config
 }
 
 install_feeds() {
